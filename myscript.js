85ee91a4f654c4ba52a52ba7bf526b6c6570eb9f
Steferre <ferrera.ste@gmail.com>
2021-04-18 17:33:47 +0200

    Progetto Completato

--- myscript.js
+++ myscript.js
@@ -10,7 +10,11 @@ Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo
 Buon lavoro e buon viaggio!
 */
 
-// per prendere i valori del form
+
+// metto tutto all'interno di una funzione IIFE per lavorare sebza variabili globali
+
+(function (){
+    // per prendere i valori del form
 // devo associare un evento al form tipo il submit 
 // e poi posso leggere i valori di ogni campo e quello che l'utente ha inserito
 
@@ -30,11 +34,11 @@ userForm.addEventListener('submit', function(event) {
 
     var formElements = form.elements;
 
-    var userName =  formElements.completeName.value;
+    var userName = formElements.completeName.value;
     var userTrip = formElements.yourTrip.value;
     var userAge = formElements.ageRange.value;
     
-
+    
     
     document.getElementById('namePassenger').innerHTML= userName;
 
@@ -51,18 +55,47 @@ userForm.addEventListener('submit', function(event) {
     
     //var ticketName = nomeUtente.toUppercase();
     createTicket();
+
+})
+
+var btnReset = document.getElementById('btnReset');
+
+btnReset.addEventListener('click', function(){
+    
+
+    btnReset.style.border = "2px solid #9C609D";
+    deleteTicket();
+
+})
+
+var btnCreateTicket = document.getElementById('btnCreateTicket');
+
+btnCreateTicket.addEventListener('click', function(){
+
+    btnCreateTicket.style.border = "2px solid #9C609D";
+
 })
 
 
 
+// FUNZIONI
 
 function  createTicket() {
     var ticket = document.getElementById('ticketData');
     if (ticket.style.display === "none") {
         ticket.style.display = "none";
+
     } else {
         ticket.style.display = "block";
+
     }
+}
+
+function  deleteTicket() {
+    var ticket = document.getElementById('ticketData');
+
+    ticket.style.display = "none";
+
 }
 
 function randomNumbers(min, max) {
@@ -73,7 +106,7 @@ function ticketCostIs(trip, age) {
     if (trip < 1) {
         alert('Devi inserire un numero maggiore di 1!')
 
-    } else if (Number.isNaN(trip)) {
+    } else if (isNaN(trip)) {
         alert('devi inserire un numero maggiore di 0!!!');
 
     } else {
@@ -100,3 +133,82 @@ function ticketCostIs(trip, age) {
     }
 
 }
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
